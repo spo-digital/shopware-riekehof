cf76b35ae089555a5cb5ec268f3d989746a3bdef
Sven Stiller <sven@s-punkt-online.de>
2025-09-24 14:56:14 +0200

    Add all

---
+++ custom/plugins/RiekehofBasicTheme/src/RiekehofBasicTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace RiekehofBasicTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class RiekehofBasicTheme extends Plugin implements ThemeInterface
+{
+}
